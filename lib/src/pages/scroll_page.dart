16501eb6d737863d6a8a81375423295d3fcdcf1c
Adilson Chameia <adilsonkchameia@outlook.com>
2022-01-16 14:29:08 +0100

    pagina1 com scroll

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class ScrollPage extends StatelessWidget {
+  //Cor de fundo
+  final Color _corFundo = const Color.fromRGBO(108, 192, 218, 1.0);
   const ScrollPage({Key? key}) : super(key: key);
 
   @override
@@ -16,14 +18,56 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1() {
-    return Center(
-      child: Text('1'),
+    //Colocar um widget emcima do outro
+    //A ordem importa
+    return Stack(
+      children: [
+        _colorFundo(),
+        _imagemFundo(),
+        _texto(),
+      ],
     );
   }
 
   _pagina2() {
-    return Center(
-      child: Text('2'),
+    return Container(
+      child: _colorFundo(),
+    );
+  }
+
+  Widget _colorFundo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: _corFundo,
+    );
+  }
+
+  Widget _imagemFundo() {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: const Image(
+        image: AssetImage('assets/scroll.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _texto() {
+    const estiloTexto = TextStyle(color: Colors.white, fontSize: 56);
+    return SafeArea(
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20.0,
+          ),
+          Text('11º', style: estiloTexto),
+          Text('Segunda-Feira', style: estiloTexto),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white),
+        ],
+      ),
     );
   }
 }
